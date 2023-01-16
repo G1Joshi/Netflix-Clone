487272a6519d67c701526aa76b405d1defefd98c
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-16 13:11:58 +0530

    initial commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jeevan Chandra Joshi on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
