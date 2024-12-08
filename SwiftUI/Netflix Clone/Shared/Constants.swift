12baffe89fefaab0d76f7e4ad72c5caae3e605c8
Jeevan Joshi <joshijeevan98@gmail.com>
2024-12-08 13:28:54 +0530

    add constant

---
+++ SwiftUI/Netflix Clone/Shared/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Netflix Clone
+//
+//  Created by Jeevan Chandra Joshi on 08/12/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let usersAPI = "https://dummyjson.com/users"
+    static let productsAPI = "https://dummyjson.com/products"
+}
